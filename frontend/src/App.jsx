762e897356c0e03af4f9c6539e9537713d6bf80a
Lijoice <lijoice03@gmail.com>
2025-03-07 12:47:45 +0530

    home page updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
+import Home from './component/Home.jsx'
+import Footer from './component/Footer.jsx'
+import Navbar from './component/NavBar.jsx'
 const App = () => {
   return (
     <div>
-      <p>hi</p>
+      <Navbar/>
+      <Home />
+      <Footer />
     </div>
   )
 }
 
 export default App
+
+
